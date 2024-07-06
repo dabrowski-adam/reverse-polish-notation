f49aba1ed00c467bdad6d028563cb46d92ba9d5b
Adam Dąbrowski <contact@adamdabrowski.com>
2024-07-06 21:40:14 +0200

    Allow running the calculator from terminal

--- src/main/scala/com/adamdabrowski/App.scala
+++ src/main/scala/com/adamdabrowski/App.scala
@@ -1,7 +1,18 @@
 package com.adamdabrowski
 
 
+import scala.util.chaining.scalaUtilChainingOps
+
+import com.adamdabrowski.Calculator.calculate
+
+
 object App:
 
   def main(args: Array[String]): Unit =
-    println("Hello, world!")
+    calculate(args.mkString(" "))
+      .map(_.toString pipe dropTrailingZeros)
+      .fold(println, println)
+
+  private def dropTrailingZeros(string: String): String =
+    if string.contains('.') then string.reverse.dropWhile(Set('0', '.').contains).reverse
+    else string
